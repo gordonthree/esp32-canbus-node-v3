861b7519edf0dcc3a6e376fbb2936bda0e1f1b63
Gordon McLellan <gordonthree@gmail.com>
2026-03-10 23:35:20 -0400

    shared API functions to allow producer functions limited access to the subModule struct

---
+++ src/node_state.cpp
@@ -0,0 +1,17 @@
+#include "node_state.h"
+
+producer_t* producerGetState(const uint8_t sub_idx) {
+    return &node.subModule[sub_idx].producer;
+}
+
+producer_cfg_t* producerGetConfig(const uint8_t sub_idx) {
+    return &node.subModule[sub_idx].producer_cfg;
+}
+
+uint8_t producerGetFlags(const uint8_t sub_idx) {
+    return node.subModule[sub_idx].flags;   // or whatever field is relevant
+}
+
+void producerSetFlags(const uint8_t sub_idx, uint8_t flags) {
+    node.subModule[sub_idx].flags = flags;
+}
