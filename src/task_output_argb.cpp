c6f733e7dbdfd5298688667b2bf90ec20060b8a3
Gordon McLellan <gordonthree@gmail.com>
2026-03-29 02:43:58 -0400

    add debug logging

--- src/task_output_argb.cpp
+++ src/task_output_argb.cpp
@@ -1,5 +1,4 @@
 #include "task_output_argb.h"
-#include "task_output_gpio.h"
 #include "argb_hw.h"
 
 #include "esp_log.h"
@@ -20,5 +19,6 @@ static const char *TAG = "task_output_argb";
 
 void argbRequestStripColor(uint8_t index, uint8_t colorIndex)
 {
+    ESP_LOGD(TAG, "Set strip %u to palette index %u", index, colorIndex);
     argbSetStripColor(index, colorIndex);
 }
